d3b84d8b7599e4fc7beb938b83fb6a4b6d3954a4
SuciuPatrick <patrick.suciu97@gmail.com>
2019-11-15 14:03:42 +0200

    no

--- Instance/config.py
+++
@@ -1,5 +0,0 @@
-SECRET_KEY='9OLWxND4o83j4K4iuopO'
-
-# Scheme: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
-DATABASE_URI = 'postgres+psycopg2://medicalis:passw0rd@medicalisdb.cagzlmlqfvpo.eu-west-3.rds.amazonaws.com/medicalisdb'
-
